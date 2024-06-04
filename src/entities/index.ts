37e1ee22901fed40b3d3dbda9e9db07b1dbd577a
DanielVrs <danielvrsdev@gmail.com>
2024-06-04 09:21:16 -0300

    feat: add entities

---
+++ src/entities/index.ts
@@ -0,0 +1,7 @@
+import Address from "./Addresses.entity";
+import Category from "./Categories.entity";
+import RealEstate from "./RealEstates.entity";
+import Schedule from "./Schedules.entity";
+import User from "./User.entity";
+
+export { Address, Category, RealEstate, Schedule, User };
